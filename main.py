872d513329c63f8cfa4f29e8f92952f08eb08435
EcoBay <ecobayod011903@gmail.com>
2021-09-29 19:07:33 +0800

    Implemented VC detection

--- main.py
+++ main.py
@@ -6,30 +6,17 @@ class DiscordBot(discord.Client):
         print("We have logged in {0.user}".format(self))
 
     async def on_message(self, message):
-        username = str(message.author).split('#')[0]
-        user_message = str(message.content)
-        channel = str(message.channel.name)
-        print(f'{username}: {user_message} ({channel})')
-
+        # Detect that it is not send by self
         if message.author == self.user:
             return
 
-        if user_message.lower() == '$hello':
-            await message.channel.send(f'Hellu {username}! ')
-            return
-        elif user_message.lower() == '$bye':
-            await message.channel.send(f'UwU {username}')
-            return
-        elif user_message.lower() == '$fuckyou':
-            await message.channel.send(f'fuck you{username}')
-            return
-        elif user_message.lower() == '$random':
-            response = f'This is ur number: {random.randrange(10000)}'
-            await message.channel.send(response)
+        # Detect if message starts with prefix
+        if not message.content.startswith("$"):
             return
 
-        if message.channel.lower() == '!anywhere':
-            await message.channel.send('Hellu I\'m here')
-            return
+        if v := message.author.voice:
+            await message.channel.send(f"{message.author.name} is at {v.channel.name}")
+        else:
+            await message.channel.send(f"{message.author.name} is not in a VC")
 
 DiscordBot().run(TOKEN)
