f66612e1bf0b6d98e9518d0e169ff3c3f9bc40c6
EcoBay <ecobayod011903@gmail.com>
2021-09-29 18:26:49 +0800

    Used class for the client

--- main.py
+++ main.py
@@ -1,42 +1,35 @@
 import discord
-import random
 from TOKEN import TOKEN
 
+class DiscordBot(discord.Client):
+    async def on_ready(self):
+        print("We have logged in {0.user}".format(self))
 
-client = discord.Client()
+    async def on_message(self, message):
+        username = str(message.author).split('#')[0]
+        user_message = str(message.content)
+        channel = str(message.channel.name)
+        print(f'{username}: {user_message} ({channel})')
 
+        if message.author == self.user:
+            return
 
-@client.event
-async def on_ready():
-    print("We have logged in {0.user}".format(client))
+        if user_message.lower() == '$hello':
+            await message.channel.send(f'Hellu {username}! ')
+            return
+        elif user_message.lower() == '$bye':
+            await message.channel.send(f'UwU {username}')
+            return
+        elif user_message.lower() == '$fuckyou':
+            await message.channel.send(f'fuck you{username}')
+            return
+        elif user_message.lower() == '$random':
+            response = f'This is ur number: {random.randrange(10000)}'
+            await message.channel.send(response)
+            return
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split('#')[0]
-    user_message = str(message.content)
-    channel = str(message.channel.name)
-    print(f'{username}: {user_message} ({channel})')
+        if message.channel.lower() == '!anywhere':
+            await message.channel.send('Hellu I\'m here')
+            return
 
-    if message.author == client.user:
-        return
-
-    if user_message.lower() == '$hello':
-        await message.channel.send(f'Hellu {username}! ')
-        return
-    elif user_message.lower() == '$bye':
-        await message.channel.send(f'UwU {username}')
-        return
-    elif user_message.lower() == '$fuckyou':
-        await message.channel.send(f'fuck you{username}')
-        return
-    elif user_message.lower() == '$random':
-        response = f'This is ur number: {random.randrange(10000)}'
-        await message.channel.send(response)
-        return
-
-    if message.channel.lower() == '!anywhere':
-        await message.channel.send('Hellu I\'m here')
-        return
-
-client.run(TOKEN)
-
+DiscordBot().run(TOKEN)
